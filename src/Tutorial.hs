44846a34c5280ab83afb13e3e7c94dbda26465f8
Ting-gian LUA <banacorn@gmail.com>
2023-02-23 15:55:34 +0800

    [ change ] Examples in Tutorial

--- src/Tutorial.hs
+++ src/Tutorial.hs
@@ -12,13 +12,33 @@ echo = do
   x <- input Private -- request for a private input and bind it to 'x'
   return x -- return 'x'
 
--- | A program that expects the second input
--- to be the square of the first input
+-- | A program that requests for 2 inputs and does nothing with them
+useless :: Comp ()
+useless = do
+  _x <- inputField Public -- request for a public input and bind it to 'x'
+  _y <- inputField Private -- request for a private input and bind it to 'y'
+  return ()
+
+-- | A program that expects the second input to be the square of the first input
 square :: Comp ()
 square = do
-  x <- inputField Private -- request for a private input and bind it to 'x'
+  x <- inputField Public -- request for a public input and bind it to 'x'
   y <- inputField Private -- request for a private input and bind it to 'y'
   assert ((x * x) `eq` y) -- assert that 'y' equals to 'x' squared
+
+-- | A stupid calculator has only 2 operations: addition and multiplication.
+--   It takes an Boolean operation flag and 2 inputs, and returns the result.
+calculator :: Comp Field
+calculator = do
+  addOrMultiply <- inputBool Public
+  x <- inputField Public
+  y <- inputField Public
+
+  return $
+    cond
+      addOrMultiply
+      (x + y)
+      (x * y)
 
 -- | A program that asserts all 10 inputs to be 42
 allBe42 :: Comp ()
@@ -48,13 +68,13 @@ birthday = do
 -- | A program that outputs the input to the 4th power (without computation reusing)
 notReused :: Comp [Field]
 notReused = do
-  x <- input Private
+  x <- input Public
   let y = x * x * x * x
   return [y, y]
 
 -- | A program that outputs the input to the 4th power (with computation reusing)
 reused :: Comp [Field]
 reused = do
-  x <- input Private
+  x <- input Public
   y <- reuse $ x * x * x * x
   return [y, y]
