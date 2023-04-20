fd8464f2fae9070dd2a57d5b9a2617ba3c168dff
Ting-gian LUA <banacorn@gmail.com>
2023-04-21 07:58:37 +0800

    [ change ] Implement the Sudoku solver with UInts

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 module Sudoku where
 
 import Control.Monad
@@ -6,24 +7,23 @@ import Keelung
 sudoku :: Comp ()
 sudoku = do
   -- each cell in the problem matrix ranges from 1 to 9, with 0 indicating an empty cell
-  problem <- inputList2 Public 9 9 :: Comp [[Field]]
+  problem <- inputList2 Public 9 9 :: Comp [[UInt 4]]
   solution <- inputList2 Private 9 9
-
-  -- check if all numbers in the problem matrix are in the range 1 to 9
-  forM_ [0 .. 8] $ \i ->
-    forM_ [0 .. 8] $ \j ->
-      assert $ from0to9 (problem !! i !! j)
 
   -- check if the solution is part of the problem:
   --    if the problem cell is not empty, then the solution cell must be equal to the problem cell
   forM_ [0 .. 8] $ \i ->
     forM_ [0 .. 8] $ \j ->
-      -- when (problem !! i !! j /= 0) $
       assert $
         cond
           (problem !! i !! j `eq` 0)
           true
           ((problem !! i !! j) `eq` (solution !! i !! j))
+
+  -- check if all numbers in the problem matrix are in the range 0 to 9
+  forM_ [0 .. 8] $ \i ->
+    forM_ [0 .. 8] $ \j ->
+      from0to9 (problem !! i !! j)
 
   -- check if each row of the solution is valid:
   forM_ [0 .. 8] $ \i ->
@@ -43,13 +43,14 @@ sudoku = do
         ]
 
 -- | Check if a list contain the numbers 1 to 9 exactly once
-valid :: [Field] -> Comp ()
+valid :: [UInt 4] -> Comp ()
 valid xs = do
   assert $ sum xs `eq` 45
   assert $ product xs `eq` 362880
 
-from0to9 :: Field -> Boolean
-from0to9 x = x `eq` 0 .|. x `eq` 1 .|. x `eq` 2 .|. x `eq` 3 .|. x `eq` 4 .|. x `eq` 5 .|. x `eq` 6 .|. x `eq` 7 .|. x `eq` 8 .|. x `eq` 9
+from0to9 :: UInt 4 -> Comp ()
+from0to9 x = assertLTE x 9
+    -- x `eq` 0 .|. x `eq` 1 .|. x `eq` 2 .|. x `eq` 3 .|. x `eq` 4 .|. x `eq` 5 .|. x `eq` 6 .|. x `eq` 7 .|. x `eq` 8 .|. x `eq` 9
 
 testProblem :: [Integer]
 testProblem =
