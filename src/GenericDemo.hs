260d64c0429fa30d043e34e578a0befc36eab720
Viktor <vic@btq.li>
2024-05-13 18:02:01 +0800

    [ add ] Generic Inputable examples

--- src/GenericDemo.hs
+++ src/GenericDemo.hs
@@ -3,7 +3,7 @@
 
 module GenericDemo where
 
-import GHC.Generics
+import GHC.Generics hiding (UInt)
 import Keelung
 
 data Enc a = A a | R (Enc a) a a
@@ -13,3 +13,26 @@ instance Encode a => (Encode (Enc a))
 
 testEnc :: Enc a -> Comp (Enc a)
 testEnc = return
+
+-- | Encoding of Either has been defined in the standard library
+-- testEither :: Either a b -> Comp (Either a b)
+-- testEither = return
+
+testMaybe :: Comp (Maybe (UInt 8, UInt 8))
+testMaybe = do dividend <- inputUInt Public :: Comp (UInt 8)
+               divisor  <- inputUInt Public :: Comp (UInt 8)
+               if divisor == 0 then
+                 return Nothing
+               else
+                 Just <$> performDivMod dividend divisor
+
+data ComplexData = Com (Pub Boolean, Prv Field) (Prv Field)
+  deriving (Generic)
+
+instance (Inputable ComplexData)
+instance (Encode ComplexData)
+
+inputComplexData :: Comp (Boolean, Field)
+inputComplexData = do
+    Com (b, f1) _ <- input' :: Comp ComplexData
+    return (getVar b, getVar f1)
